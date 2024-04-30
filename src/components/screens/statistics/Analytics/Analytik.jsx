af4438b2f8ec4cb615c78557701895e84911cf20
NLastya <nlastya1994@mail.ru>
2024-05-01 02:14:23 +0300

    added localStorage

--- src/components/screens/statistics/Analytics/Analytik.jsx
+++ src/components/screens/statistics/Analytics/Analytik.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import ReportDiv from '../4/ReportDiv'
+import ReportDiv from '../ReportDiv'
 // import <CChart> from '@cu'
-import ScrollingCharts from './scrollDiagrams'
+import ScrollingCharts from '../scrollDiagrams'
 import DiagramPercent from './DiagramPercent'
-import BarsDiagram from './BarDiagram'
-import PieChartWithLabel from './PieChartWithLabel'
-// import 
+import LinearChart from './LinearDiagramm.tsx'
+import BarsDiagram from './BarDiagram.tsx'
+import PieChartWithLabel from './PieChartWithLabel.tsx'
 
 const ThirdTab = () => {
 
@@ -26,20 +26,20 @@ const pieChartData =  [ { value: 30, label: "Хорошо", color: "rgba(223, 23
 
   return (
     <div className="FirstTab">
-      <div className="report__section" style={{"height": "140px", "marginBottom": "40px"}}>
-      <ReportDiv label="Средний балл выгорания" width="146px" height="139px"><DiagramPercent percent={54}/></ReportDiv>
+      <div className="report__section" style={{"height": "140px", "margin-bottom": "40px"}}>
+      <ReportDiv label="Средний балл выгорания" width="146px" height="139px"><DiagramPercent/></ReportDiv>
       <ReportDiv label="Аналитика по филиалам" width="442px" height="139px"><ScrollingCharts data={data}/></ReportDiv>
       <ReportDiv label="Аналитика по должностной категории" width="442px" height="139px"><ScrollingCharts data={data}/></ReportDiv>
       </div>
 
       <div className="report__section" style={{"height": "279px"}}>
       <ReportDiv label="Сравнительный анализ" width="628px" height="279px"><BarsDiagram width={628} height={279}/></ReportDiv>
-      <ReportDiv label="Аналитика по стажу работы" width="201px" height="279px"><PieChartWithLabel data={pieChartData} label={54} width={201} height={279} pos=""/></ReportDiv>
+      <ReportDiv label="Аналитика по стажу работы" width="201px" height="279px"><PieChartWithLabel data={pieChartData} label={54} width={201} height={279}/></ReportDiv>
       <ReportDiv label="Аналитика по фазам" width="201px" height="279px"><>
-      <div className="inner__repoprt__div" style={{"backgroundImage": "url('./Bubbles.svg'"}}>
-        <p style={{"fontSize": "9px"}}>Истощение</p><p style={{"marginLeft": "auto", "fontSize": "9px"}}>54</p>
-        <p style={{"fontSize": "9px"}}>Деперсонализация</p><p style={{"marginLeft": "auto", "fontSize": "9px"}}>12</p>
-        <p style={{"fontSize": "9px"}}>Редукция</p><p style={{"marginLeft": "auto", "fontSize": "9px"}}></p>
+      <div className="inner__repoprt__div">
+        <p>Истощение</p><p style={{"margin-left": "auto"}}>54</p>
+        <p>Деперсонализация</p><p style={{"margin-left": "auto"}}>12</p>
+        <p>Редукция</p><p style={{"margin-left": "auto"}}></p>
       </div>
 
         </></ReportDiv>
--- src/components/screens/survey/SurveyContainer.tsx
+++ src/components/screens/survey/SurveyContainer.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react'
+import { FC, useState, useEffect } from 'react'
 import quizData from './Questions/questions.json'
 import styles from './Survey.module.css'
 import SurveyHeading from './SurveyHeading'
@@ -13,21 +13,107 @@ const SurveyContainer: FC = () => {
 
 	const keys = Object.keys(data)
 	const numberOfFields = keys.length
-	const [selectedButtons, setSelectedButtons] = useState(
-		new Array(numberOfFields + 1).fill('none')
-	)
+
+	const [selectedButtons, setSelectedButtons] = useState(() => {
+		// localStorage.setItem('selectedButton', 'nnnnnnnnnnnnnnnnnnnnnnn')          //  <------ сброс localStorage
+
+		const storedButton = localStorage.getItem('selectedButton')
+		if (storedButton) {                                                                         // закомментить, чтобы работать без LocalStorage
+		return Array.from(storedButton)																// закомментить, чтобы работать без LocalStorage
+		} else {																					// закомментить, чтобы работать без LocalStorage
+		const numberOfFields = keys.length	
+		localStorage.setItem('answer', 'nnnnnnnnnnnnnnnnnnnnnnn')													
+		return new Array(numberOfFields + 1).fill('n')
+		}																							// закомментить, чтобы работать без LocalStorage
+	})
+
 
 	const [answer, setAnswer] = useState(() => {
+		const storedAnswer = localStorage.getItem('answer');
+		const parsedAnswer = Object.keys((JSON.parse(storedAnswer)).length !== 0) ? JSON.parse(storedAnswer) : 0;
+		// console.log(storedAnswer)
+		if(!parsedAnswer){
+			console.log("sheet, yes")
 		const answerMap = new Map(Object.entries(data))
-
 		answerMap.forEach((value, key) => {
 			const type = parseInt(value.trend)
 			const points = 0
 			answerMap.set(key, { type, points })
 		})
 
+		let objAnsw = {}
+		for(let item of answerMap.keys()){
+			objAnsw[item] = answerMap.get(item)
+		}
+
+		console.log(objAnsw)
+		console.log(JSON.stringify(objAnsw))
+		localStorage.setItem('answer', JSON.stringify(objAnsw))
+		console.log(localStorage)
 		return answerMap
+	} else {
+		let parsedMap = new Map()
+		let back = parsedAnswer
+		for(let prop in back){
+			parsedMap.set(prop, back[prop])
+		}
+		return parsedMap
+	}
 	})
+	console.log("answer: ", answer)
+
+	const initialAnswer = new Map(Object.entries(data))
+	initialAnswer.forEach((value, key) => {
+		const type = parseInt(value.trend)
+		const points = 0
+		initialAnswer.set(key, { type, points })})
+
+	let initialObjAnswer = {}
+		for(let item of initialAnswer.keys()){
+			initialObjAnswer[item] = initialAnswer.get(item)
+		}
+
+ 
+	
+
+	// useEffect(()=>{}, [answer])
+
+	useEffect(() => {
+		if(selectedButtons !== Array.from('nnnnnnnnnnnnnnnnnnnnnnn')){
+			console.log("selected button [selectedButton]")
+			localStorage.setItem('selectedButton', (selectedButtons.map(item => {if(item !== 'n') return`${item}`; else return "n"})).join(''))
+		}
+		console.log(localStorage)
+	}, [selectedButtons]) 
+
+
+	useEffect(() => {
+
+		let objAnsw = {}
+		for(let item of answer.keys()){
+			objAnsw[item] = answer.get(item)
+		}
+
+		console.log("initialMap: ", initialObjAnswer)
+		if(JSON.stringify(objAnsw) !== JSON.stringify(initialObjAnswer)){
+			console.log("yes")
+			localStorage.setItem('answer', JSON.stringify(objAnsw))
+			console.log("selected button [answer]")
+			console.log(localStorage)
+		}
+
+	}, [answer])
+
+
+	useEffect(()=>{
+		fetch('', {
+			method: "GET",
+			headers: {
+				'Content-Type': 'application/json;charset=utf-8'
+			},
+
+		})
+	}, [])
 
 	function handleSend() {
 		const answer = calcAnswer()
@@ -76,7 +162,7 @@ const SurveyContainer: FC = () => {
 		return <div>Loading...</div>
 	} else {
 		return (
-			<>
+			<div className="containeer__survey">
 				<SurveyHeading />
 				<div className={styles.surveyContainer}>
 					{!isResult && (
@@ -101,7 +187,7 @@ const SurveyContainer: FC = () => {
 					)}
 					{isResult && <Result answer={finalAnswer} />}
 				</div>
-			</>
+			</div>
 		)
 	}
 }
